01bf93d1d65f79f6e9c42c769ff6736017dda633
Emmanuel Omemgboji <emmanuelomemgboji@gmail.com>
2022-12-13 08:57:07 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+// import { MailchimpForm } from "./components/MailchimpForm";
+import { Footer } from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
